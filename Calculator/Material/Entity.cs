6c420d09970a7aae0a6366eae8b7fbf760f71dd4
lovcavil <lovcavil@hotmail.com>
2015-09-15 01:33:23 +0800

    update

---
+++ Calculator/Material/Entity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StupidMonkey.Material
+{
+    class Entity
+    {
+        float Mass;
+        Material material;
+        Structure structure;
+
+        Entity[] list;
+    }
+}
