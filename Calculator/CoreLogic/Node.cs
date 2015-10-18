b575427de0cd9aeabc9377345bd2c8fcf5ce2218
lovcavil <lovcavil@hotmail.com>
2015-10-19 01:18:24 +0800

    111

---
+++ Calculator/CoreLogic/Node.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StupidMonkey.CoreLogic
+{
+    /// <summary>
+    /// 预计拿来处理保存update initial的功能
+    /// 
+    /// </summary>
+    class Node
+    {
+        int id;
+        INodetype nt;
+        public void Update()
+        {
+            nt.Update();
+        }
+    }
+    interface INodetype
+    {
+         void Update();
+    }
+}
